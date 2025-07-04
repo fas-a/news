4f996dd5a83571dd664b2cae13419674c29daed7
fas-a <abulkhoirfaris0@gmail.com>
2025-07-04 20:06:59 +0700

    update :  environtment python

--- back_end/configuration.py
+++ back_end/configuration.py
@@ -12,3 +12,4 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 db = client[os.getenv("DB_NAME")]
 cost_factor = int(os.getenv("COST_FACTOR"))
 secret_key = os.getenv("SECRET_KEY")
+cors = os.getenv("CORS")
--- back_end/main.py
+++ back_end/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.news import router as news_router
 from api.auth import router as auth_router
+from configuration import cors
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[cors],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
